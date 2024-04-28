aa56ff849d8098cf9623779fa23c5ebb5a794ec4
JNTimmers <167239244+JNTimmers@users.noreply.github.com>
2024-04-28 14:25:35 +0200

    FightScene

--- src/main/java/brawlhalla/player/ControlledPlayerWSDA.java
+++ src/main/java/brawlhalla/player/ControlledPlayerWSDA.java
@@ -17,19 +17,47 @@ public class ControlledPlayerWSDA extends Player {
 
     @Override
     public void onPressedKeysChange(Set<KeyCode> pressedKeys){
-        if(pressedKeys.contains(KeyCode.A)){
-            setMotion(3,270d);
-        } else if(pressedKeys.contains(KeyCode.D)){
-            setMotion(3,90d);
-        } else if(pressedKeys.contains(KeyCode.W) && isGrounded){
+        if (pressedKeys.contains(KeyCode.W) && isGrounded) {
             setIsGrounded(false);
-            setMotion(5,180d);
-        } else if(pressedKeys.contains(KeyCode.S) && !isGrounded){
-            setMotion(3,0d);
+            setMotion(5, 180d);
+        } else if (pressedKeys.contains(KeyCode.S) && !isGrounded) {
+            setMotion(3, 0d);
+        } else if (pressedKeys.contains(KeyCode.A)) {
+
+            if (isGrounded) {
+                setMotion(3, 270d);
+            } else if (!isGrounded && getDirection() == 315d) {
+                setMotion(3, 315d);
+            } else if (!isGrounded) {
+                if (getDirection() == 0) {
+                    setMotion(3, 355);
+                } else if (getDirection() < 315d && getDirection() >= 180d) {
+                    setMotion(3, getDirection() + 5);
+                } else if (getDirection() > 315d || getDirection() < 180d) {
+                    setMotion(3, getDirection() - 5);
+                }
+            }
+        } else if (pressedKeys.contains(KeyCode.D)) {
+
+            if (getDirection() == 360d) {
+                setDirection(0d);
+            }
+            if (isGrounded) {
+                setMotion(3, 90d);
+            } else if (!isGrounded && getDirection() == 45d) {
+                setMotion(3, 45d);
+            } else if (!isGrounded) {
+                if (getDirection() < 45d || getDirection() > 180d) {
+                    setMotion(3, getDirection() + 5);
+                } else if (getDirection() > 45d && getDirection() <= 180d) {
+                    setMotion(3, getDirection() - 5);
+                }
+            }
         }
 
         // Do attack
-        if(pressedKeys.contains(KeyCode.Q)) {
+        if (pressedKeys.contains(KeyCode.CONTROL)) {
+            System.out.println("attack! ");
             weapon.attack(getDirection());
         }
     }
