eaf632038ee395d2cc9f320bd273ab9ca24d8b53
ReinoutWW <71006243+ReinoutWW@users.noreply.github.com>
2024-05-01 20:31:17 +0200

    IWeapon

--- src/main/java/brawlhalla/weapons/IWeapon.java
+++ src/main/java/brawlhalla/weapons/IWeapon.java
@@ -1,25 +1,4 @@
-package brawlhalla.weapons;
-
-import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.entities.YaegerEntity;
+package brawlhalla.Weapons;
 
 public interface IWeapon {
-    int attackSpeedCooldown = 0;
-    boolean isHeldByCharacter = false;
-
-    void setReadyForAttack(boolean ready);
-
-    void attack(double direction, Coordinate2D startPosition);
-
-    /**
-     * @return the damage increate in percentages.
-     * For example: 10 = 10% increase
-     */
-    int getDamage();
-
-    /**
-     * @return the default knockback performed in speed.
-     * For example: 1 = 1 speed in yaeger
-     */
-    int getKnockback();
 }
