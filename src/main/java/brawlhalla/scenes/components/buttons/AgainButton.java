6c3705460f7eaa11218c97843351fab5e57e7d4b
JNTimmers <167239244+JNTimmers@users.noreply.github.com>
2024-05-01 20:40:29 +0200

    Create againButton

---
+++ src/main/java/brawlhalla/scenes/components/buttons/AgainButton.java
@@ -0,0 +1,42 @@
+package brawlhalla.scenes.components.buttons;
+
+import brawlhalla.Brawhalla;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
+import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
+import com.github.hanyaeger.api.userinput.MouseEnterListener;
+import com.github.hanyaeger.api.userinput.MouseExitListener;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class AgainButton extends TextEntity implements MouseButtonPressedListener, MouseEnterListener, MouseExitListener {
+    private Brawhalla brawhalla;
+
+    public AgainButton(Coordinate2D initialLocation, Brawhalla brawhalla, String text) {
+        super(initialLocation);
+        this.brawhalla = brawhalla;
+        setText(text);
+        setFill(Color.PURPLE);
+        setFont(Font.font("Roboto", FontWeight.BOLD, 30));
+    }
+
+    @Override
+    public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
+        brawhalla.setActiveScene(0);
+    }
+
+    @Override
+    public void onMouseEntered() {
+        setFill(Color.VIOLET);
+        setCursor(Cursor.HAND);
+    }
+
+    @Override
+    public void onMouseExited(){
+        setFill(Color.PURPLE);
+        setCursor(Cursor.DEFAULT);
+    }
+}
